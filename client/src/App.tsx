ae8abc75ef4210cfaf78cf397a3a2bb05e7ae9c3
Jacob Millner <jacob.millner@outlook.com>
2021-02-06 17:18:27 -0500

    feat: adding material ui component library, .prettierrc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import LoginIcon from '@material-ui/icons/AccountCircle';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button startIcon={<LoginIcon />} color="primary" variant="contained">
+        Login
+      </Button>
     </div>
   );
 }
